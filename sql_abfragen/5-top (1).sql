238530eb41dfb0794ce311c5571ed58d97f48705
T-Svitlichna <svitlichnaya@gmail.com>
2026-07-27 11:11:59 +0200

    Delete sql_abfragen/5-top (1).sql

--- sql_abfragen/5-top (1).sql
+++
@@ -1,5 +0,0 @@
-select o.customer_id, c.region, c.age, o.net_amount, c.acquisition_chan, o.free_shipping 
-from shopsphere_orders as o join shopsphere_customers as c USING (customer_id)
-WHERE o.is_returned = 0
-
-
